39746d58b6526b84098077910e4ef959950ca268
biancademambro <biancademambro12@hotmail.com>
2026-06-23 20:11:43 -0300

    Alteração de ícone do app na janela e tarefa

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("path")
 
 function criarJanela() {
     const janela = new BrowserWindow({
         width: 1200,
-        height: 800
+        height: 800,
+        icon: path.join(__dirname, "logo", "logo.png")
     })
 
     janela.loadFile("public/login.html")
 }
 
+if (process.platform === "win32") {
+    app.setAppUserModelId("com.streaming.app")
+}
+
 app.whenReady().then(criarJanela)
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        criarJanela()
+    }
+})
